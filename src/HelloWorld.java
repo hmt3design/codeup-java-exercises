01f2efd6cae64de0fd437f275559cda6fe41e985
hmt3design <hmt3design@gmail.com>
2017-01-03 14:28:19 -0600

    Initial upload of Java exercises, 010317

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+/**
+ * Created by Harry on 1/3/17 as initial file check
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("All java code lives inside of classes");
+        System.out.println("Our program runs the 'main' method when executed");
+        System.out.println("Goodbye, cruel world!");
+    }
+}
