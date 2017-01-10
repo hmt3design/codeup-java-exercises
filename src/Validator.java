97267888895e920edec3df08e4905692dfbc03e5
hmt3design <hmt3design@gmail.com>
2017-01-10 11:35:26 -0600

    Sixth day of Java exercises, 011017 (before leaving for dentist)

--- src/Validator.java
+++ src/Validator.java
@@ -1,9 +1,32 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 /**
  * Created by Harry on 1/9/17.
  */
 public class Validator {
-    int getInt;
-    double getDouble;
-    String getRequiredString;
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
 
+    public int getInt(String prompt, Scanner scan) {
+        int userInteger;
+        System.out.print(prompt);
+        try {
+            userInteger = scan.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Please enter an integer.");
+            scan.next();
+            return getInt(prompt, scan);
+        }
+        return userInteger;
+    }
+
+//    public int getIntWithinRange(String prompt, int min, int max) {
+//        int userInteger;
+//        System.out.print(prompt);
+//
+//    }
 }
+
---
+++ src/ValidatorTestApp.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Harry on 1/10/17.
+ */
+public class ValidatorTestApp {
+    public static void main(String[] args) {
+        System.out.println("Integer test:");
+        System.out.println("Please enter an integer:");
+
+    }
+}
